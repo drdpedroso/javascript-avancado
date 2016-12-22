7e9f68af03f3f2e189e188222df4cff508443ee8
Eduardo Pedroso <eduardo.pedroso@matera.com>
2016-12-22 18:33:16 -0200

    Safe

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -3,19 +3,19 @@ class NegociacaoController {
   constructor(){
     let $ = document.querySelector.bind(document);
 
-    this.inputData = $('#data');
-    this.inputQuantidade = $('#quantidade');
-    this.inputValor = $('#valor');
+    this._inputData = $('#data');
+    this._inputQuantidade = $('#quantidade');
+    this._inputValor = $('#valor');
 
   }
 
   adiciona(event){
     event.preventDefault();
-
-
-        console.log(this.inputValor.value);
-        console.log(this.inputQuantidade.value);
-        console.log(this.inputData.value);
+    let negociacao = new Negociacao(
+        this._inputData.value
+      , this._inputQuantidade.value
+      , this._inputValor.value
+    )
 
   }
 
